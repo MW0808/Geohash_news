68f08b1f596912ed2342f720a25281dbd57a9eb8
Chieh-yi Wu <minaw.wu@mail.utoronto.ca>
2025-01-18 04:38:31 -0500

    moved report model to src

---
+++ backend/src/report.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const reportSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        location: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        upvote: {
+            type: Number,
+            default: 0
+        },
+        downvote: {
+            type: Number,
+            default: 0
+        },
+        image: {
+            type: String,
+            default: ""
+        }
+    },
+    {timestamp: true}
+);
+
+const Report = mongoose.model("Report", reportSchema);
+
+export default Report;
