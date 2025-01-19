f163bde03ce9802eaeb57f6b740776f3cd7f0bb8
Anthony <antmskim@my.yorku.ca>
2025-01-19 06:08:51 -0500

    final update

--- frontend/src/pages/NewsletterPage.jsx
+++ frontend/src/pages/NewsletterPage.jsx
@@ -4,21 +4,31 @@ import { useAuthStore } from "../store/useAuthStore";
 import { useReportStore } from "../store/useReportStore";
 
 const NewsletterPage = () => {
-
-  const {geohash} = useAuthStore();
+  const { geohash } = useAuthStore();
   const [currGeohash, setCurrGeohash] = useState(geohash);
-  const {loadedNewsletter, getNewsletter} = useReportStore();
+  const { loadedNewsletter, getNewsletter } = useReportStore();
 
   useEffect(() => {
-    getNewsletter({location: location, date: new Date()});
-  }, [loadedNewsletter, getNewsletter])
+    setCurrGeohash(geohash);
+  }, [geohash]);
+
+
+  useEffect(() => {
+    getNewsletter({ location: currGeohash, date: new Date() });
+  }, [currGeohash, getNewsletter]);
 
   return (
     <div className="min-h-screen bg-base-200 p-4">
       <h1 className="text-2xl font-bold mb-4">Newsletter</h1>
       <MapComponent />
-      <h1>{loadedNewsletter.data.title}</h1>
-      <p>{loadedNewsletter.data.content}</p>
+      {loadedNewsletter ? (
+        <>
+          <h1>{loadedNewsletter.data.title}</h1>
+          <p>{loadedNewsletter.data.content}</p>
+        </>
+      ) : (
+        <p>Loading newsletter...</p>
+      )}
     </div>
   );
 };
