0c20eec65d82ee6ececfbfd75dfd18411b797b78
Anthony <antmskim@my.yorku.ca>
2025-01-19 04:36:27 -0500

    Sorting fixed

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,34 +1,33 @@
-import Report from '../components/Report'
-import Bottombar from '../components/Bottombar'
+import Report from '../components/Report';
+import Bottombar from '../components/Bottombar';
 import { useReportStore } from '../store/useReportStore';
 import { useEffect } from 'react';
 
 const HomePage = () => {
-  const {reports, getReports, getGeoHash, upvote, downvote, subscribeToReports} = useReportStore();
+  const { reports, getReports, getGeoHash, subscribeToReports } = useReportStore();
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(async (position) => {
       const lat = position.coords.latitude;
       const long = position.coords.longitude;
-      await getGeoHash(lat, long); 
+      await getGeoHash(lat, long);
       getReports();
     });
     subscribeToReports();
   }, [getGeoHash, getReports, subscribeToReports]);
-  
-  
+
+  const sortedReports = [...reports].sort((a, b) => b.score - a.score);
+
   return (
     <div className="flex flex-col items-center bg-base-200 px-4 pt-8 pb-16 space-y-6">
-    <div className="w-full md:w-3/5 space-y-4">
-      {reports.map((report) => (
-        <Report key={report._id} {...report} />
-      ))}
-    </div>
-  
+      <div className="w-full md:w-3/5 space-y-4">
+        {sortedReports.map((report) => (
+          <Report key={report._id} {...report} />
+        ))}
+      </div>
+
       <Bottombar />
     </div>
-  
-    
   );
 };
 
